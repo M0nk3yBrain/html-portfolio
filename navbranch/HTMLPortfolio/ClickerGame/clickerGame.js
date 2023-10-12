bc629168ffe92cf72731c5b8308f6757802cf64e
Jonathan Melchor <145809912+M0nk3yBrain@users.noreply.github.com>
2023-10-12 15:53:25 -0500

    Add files via upload
    HTML- Upgraded game by adding two upgrade buttons. Javascript- Updated by changing score from incremental additive to creating variable x. Score upgrade by 5 and 10.

--- navbranch/HTMLPortfolio/ClickerGame/clickerGame.js
+++ navbranch/HTMLPortfolio/ClickerGame/clickerGame.js
@@ -1,3 +1,6 @@
+var score = 0;
+let x = 1;
+
 function drawNumber(number, ctx, canvas) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -8,9 +11,27 @@ function drawNumber(number, ctx, canvas) {
     ctx.fillText(number, canvas.width /2, canvas.height /2);
 }
 
-var score = 0;
-
 function updateNumber() {
-    score++;
+    score +=x;
     drawNumber(score, ctx, canvas);
+}
+
+function upgradeScore5() {
+    if (score >= 10) {
+        x += 5;
+        score -= 10;
+        drawNumber(score, ctx, canvas);
+    } else {
+        alert("You need at least 10 points to purchase.");
+    }
+}
+
+function upgradeScore10() {
+    if (score >= 100) {
+        x += 10;
+        score -= 100;
+        drawNumber(score, ctx, canvas);
+    } else {
+        alert ("You need at least 100 point to purchase.");
+    }
 }
