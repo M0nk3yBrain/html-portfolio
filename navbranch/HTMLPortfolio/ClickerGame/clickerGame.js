b8a44a5ec7324a2700d842d7615c43b00d4c458c
Jonathan Melchor <145809912+M0nk3yBrain@users.noreply.github.com>
2023-10-15 15:35:28 -0500

    Add files via upload
    Updated the page format and layout. Reworked the entire project.

--- navbranch/HTMLPortfolio/ClickerGame/clickerGame.js
+++ navbranch/HTMLPortfolio/ClickerGame/clickerGame.js
@@ -1,37 +1,66 @@
 var score = 0;
 let x = 1;
 
-function drawNumber(number, ctx, canvas) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "blue";
-    ctx.textAlign = "center";
-
-    ctx.fillText(number, canvas.width /2, canvas.height /2);
-}
-
 function updateNumber() {
     score +=x;
-    drawNumber(score, ctx, canvas);
+    document.getElementById("showScore").innerText = score;
 }
 
-function upgradeScore5() {
+
+/*
+function startUpdating() {
+    // Start updating the score when the button is pressed
+    updateNumber(); // Initial update
+    updateInterval = setInterval(updateNumber, 100); // Update every 100ms
+}
+
+function stopUpdating() {
+    // Stop updating the score when the button is released or the cursor leaves the button
+    clearInterval(updateInterval);
+}
+*/
+
+//Upgrade buttons.
+function upgradeScore1() {
     if (score >= 10) {
-        x += 5;
+        x += 1;
         score -= 10;
-        drawNumber(score, ctx, canvas);
+        document.getElementById("showScore").innerText = score;
+        document.getElementById("showPointsPerClick").innerText = x;
     } else {
         alert("You need at least 10 points to purchase.");
     }
 }
 
-function upgradeScore10() {
+function upgradeScore2() {
     if (score >= 100) {
-        x += 10;
+        x += 2;
         score -= 100;
-        drawNumber(score, ctx, canvas);
+        document.getElementById("showScore").innerText = score;
+        document.getElementById("showPointsPerClick").innerText = x;
     } else {
-        alert ("You need at least 100 point to purchase.");
+        alert ("You need at least 100 points to purchase.");
+    }
+}
+
+function upgradeScore5() {
+    if (score >= 1000) {
+        x += 2;
+        score -= 1000;
+        document.getElementById("showScore").innerText = score;
+        document.getElementById("showPointsPerClick").innerText = x;
+    } else {
+        alert ("You need at least 1,000 points to purchase.");
+    }
+}
+
+function upgradeScore10() {
+    if (score >= 10000) {
+        x += 2;
+        score -= 10000;
+        document.getElementById("showScore").innerText = score;
+        document.getElementById("showPointsPerClick").innerText = x;
+    } else {
+        alert ("You need at least 10,000 points to purchase.");
     }
 }
