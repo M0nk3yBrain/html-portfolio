1012a4db0be7834154ab5a6dd16f2ad9e46ff6be
Jonathan Melchor <145809912+M0nk3yBrain@users.noreply.github.com>
2023-10-19 10:59:21 -0500

    Add files via upload
    Adding the beginnings of the js

---
+++ navbranch/HTMLPortfolio/starWarsJeopardy/questionScript.js
@@ -0,0 +1,252 @@
+//Questions for level 1
+const questions100 = [
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+]
+//Questions for level 1
+const questions2 = [
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+]
+//Questions for level 3
+const questions3 = [
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+]
+//Questions for level 4
+const questions4 = [
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: ["4", "3", "5", "6"],
+        correctAnswer: 0        
+    },
+]
